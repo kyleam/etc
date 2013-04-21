ced0cb167bd0c4040fa9cf4987f0c760e4cece7d
Kyle Meyer <meyerkya@gmail.com>
2013-04-21 00:47:40 -0400

    fix topmem, topcpu typo

--- system/system.zsh
+++ system/system.zsh
@@ -3,9 +3,9 @@ alias off='sudo poweroff'
 alias reboot='sudo reboot'
 
 #top 10 memory consuming process
-alias topmem="ps axo pid,pcpu,pmem,comm --sort=-rss | head -20"
+alias topmem="ps axo pid,pcpu,pmem,comm --sort=-rss | head -11"
 ##top 10 CPU consuming process
-alias topcpu="ps axo pid,pcpu,pmem,comm --sort=-pcpu| head -20"
+alias topcpu="ps axo pid,pcpu,pmem,comm --sort=-pcpu| head -11"
 
 alias tg="ping -c2 www.google.com"
 
