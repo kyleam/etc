fe31a961562a1f18066e837523a13df2a3df5525
Kyle Meyer <meyerkya@gmail.com>
2013-04-21 19:01:18 -0400

    pandas hh: correct docstring, use 5 rows and cols

--- python/ipython/profile_default/startup/funcs.py
+++ python/ipython/profile_default/startup/funcs.py
@@ -1,7 +1,7 @@
 def hh(df):
-    """View selected rows form data frame
+    """View first five rows and columns of a data frame
 
     This is useful to preview data frames that are too wide for the
     `head` output to fit on the screen
     """
-    return df.iloc[0:6, 0:6]
+    return df.iloc[0:5, 0:5]
