4d1b9e415bcf90db6e4654a1b697322d01469fb9
Kyle Meyer <meyerkya@gmail.com>
2013-04-21 17:50:40 -0400

    rename pandas view func and use iloc
    rename to be shorter
    
    don't give option for number of rows and cols (would just use icol
    itself at that point)
    
    don't check that data frame is larger than 5x5

--- python/ipython/profile_default/startup/funcs.py
+++ python/ipython/profile_default/startup/funcs.py
@@ -1,12 +1,7 @@
-def view(df, row=6, col=6):
+def hh(df):
     """View selected rows form data frame
 
     This is useful to preview data frames that are too wide for the
     `head` output to fit on the screen
     """
-    ## make sure defaults don't exceed actual shape
-    if df.shape[0] < row:
-        row = df.shape[0]
-    if df.shape[1] < col:
-        col = df.shape[1]
-    return df.ix[0:row, 0:col]
+    return df.iloc[0:6, 0:6]
