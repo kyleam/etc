ebdf5c0ce5b9f05a74d7f7f4d091b5b6a34aa1a8
Kyle Meyer <meyerkya@gmail.com>
2013-05-07 00:46:03 -0400

    ipython startup: try/except cpickle import
    py2/3 compatibility

--- python/ipython/profile_default/startup/base-import.py
+++ python/ipython/profile_default/startup/base-import.py
@@ -1,7 +1,11 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import cPickle as pickle
+
+try:
+    import cPickle as pickle
+except ImportError:
+    import pickle
 
 from colors import bgry, fgry, brew, barcolors
 import pltaid
