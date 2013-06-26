a448a4bf07ee1fdd8557a403d6f5b2959721736f
Kyle Meyer <meyerkya@gmail.com>
2013-06-26 11:26:52 -0400

    update python config for kmlab arch

--- python/python.zsh
+++ python/python.zsh
@@ -1,3 +1,16 @@
 if [[ $PYTHONPATH != $HOME/lib/python:* ]]; then
    export PYTHONPATH=$HOME/lib/python:$PYTHONPATH
 fi
+
+if [ $(whoami) != "root" ]; then
+    export WORKON_HOME=$HOME/.pyenv
+    source $(which virtualenvwrapper.sh) > /dev/null
+fi
+
+alias ipy='ipython'
+
+alias pt2='py.test2'
+alias pt='py.test'
+
+alias ns2='nosetests2'
+alias ns='nosetests'
