72b6ef988b39e6f1cf515f696c662c1e2e85112a
Kyle Meyer <meyerkya@gmail.com>
2013-06-26 11:38:13 -0400

    update etc font links
    - copy droid conf to conf.avail in addition to linking
    - link no-bitmaps even if yes-bitmaps is not in conf.d

--- font/setup-fonts.sh
+++ font/setup-fonts.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-ln -s ~/etc/os-config/font-tweaks/35-droid.conf /etc/fonts/conf.d
 
-rm /etc/fonts/conf.d/70-yes-bitmaps.conf &&
-    ln -s /etc/fonts/conf.avail/70-no-bitmaps.conf /etc/fonts/conf.d/
+cp $PWD/35-droid.conf /etc/fonts/conf.avail/
+ln -s /etc/fonts/conf.avail/35-droid.conf /etc/fonts/conf.d/
+
+rm /etc/fonts/conf.d/70-yes-bitmaps.conf
+ln -s /etc/fonts/conf.avail/70-no-bitmaps.conf /etc/fonts/conf.d/
