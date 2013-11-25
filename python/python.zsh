6a687f2bfcc905119e375db70e68bbcf035119a3
Kyle Meyer <kyle@kyleam.com>
2013-11-25 13:59:43 -0500

    Python setup.py aliases

--- python/python.zsh
+++ python/python.zsh
@@ -17,3 +17,8 @@ alias ptd2='py.test2 --doctest-modules'
 
 alias ns2='nosetests2'
 alias ns='nosetests'
+
+alias psiu2='python2 setup.py install --user'
+alias psdu2='python2 setup.py develop --user'
+alias psiu='python setup.py install --user'
+alias psdu='python setup.py develop --user'
