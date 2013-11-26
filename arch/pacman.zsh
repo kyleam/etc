2e15fa9cc2d8946279dab38a9c5c2e9d8862c7d4
Kyle Meyer <kyle@kyleam.com>
2013-11-25 22:46:17 -0500

    Update makepkg aliases

--- arch/pacman.zsh
+++ arch/pacman.zsh
@@ -2,7 +2,6 @@ alias pac='sudo pacman -S'
 alias pacu='sudo pacman -Syu'
 alias pacr='sudo pacman -Rs'  # remove deps no longer needed
 alias pacc='sudo pacman -Sc' # clean cached
-alias pacm='makepkg -fci'
 
 alias pacs='pacman -Ss'
 alias paci='pacman -Si'
@@ -11,4 +10,6 @@ alias pacll='pacman -Qqm' # list local
 alias paclo='pacman -Qdt' # list orphans
 alias paco='pacman -Qo' # owner of given file
 alias pacf='pacman -Ql' # all files by given package
+
 alias mps='makepkg -s'
+alias mpsi='makepkg -si'
