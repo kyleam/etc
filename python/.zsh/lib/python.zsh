78f8c520230ac54423398502d97d6434c3368d48
Kyle Meyer <kyle@kyleam.com>
2014-02-22 00:25:07 -0500

    Update Python 3 nose alias

--- python/.zsh/lib/python.zsh
+++ python/.zsh/lib/python.zsh
@@ -16,7 +16,7 @@ alias ptd='py.test --doctest-modules'
 alias ptd2='py.test2 --doctest-modules'
 
 alias ns2='nosetests2'
-alias ns='nosetests'
+alias ns='nosetests3'
 
 alias psiu2='python2 setup.py install --user'
 alias psdu2='python2 setup.py develop --user'
