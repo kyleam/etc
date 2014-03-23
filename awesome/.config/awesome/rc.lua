7819b27ce800401d0e5b3857afdd909f385b6e2d
Kyle Meyer <kyle@kyleam.com>
2014-03-23 18:25:44 -0400

    Rebind awful.tag.history.restore

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -214,7 +214,7 @@ music_next = "mpc next"
 globalkeys = awful.util.table.join(
     awful.key({ modkey,           }, "Left",   awful.tag.viewprev       ),
     awful.key({ modkey,           }, "Right",  awful.tag.viewnext       ),
-    awful.key({ modkey,           }, "Escape", awful.tag.history.restore),
+    awful.key({ modkey,           }, "p",      awful.tag.history.restore),
 
     awful.key({ modkey,           }, "j",
         function ()
