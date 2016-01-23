237183afe4a713434b16a008f07f94df75d95ac2
Kyle Meyer <kyle@kyleam.com>
2016-01-23 00:03:11 -0500

    Add monitor aliases

--- h-kmlab-shell/.zsh/lib/lab.zsh
+++ h-kmlab-shell/.zsh/lib/lab.zsh
@@ -1,1 +1,4 @@
 PS1="%{$fg[blue]%}[%{$reset_color%}%m: %c%{$fg[blue]%}]%{$reset_color%}%# "
+
+alias onemon="xrandr --output DVI-1 --off"
+alias twomon="xrandr --output DVI-1 --mode 1280x1024 --left-of DVI-0"
