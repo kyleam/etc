80f1751d482dc211114330a6755bfc0d81f8d84b
Kyle Meyer <kyle@kyleam.com>
2016-02-12 22:05:23 -0500

    Switch default font

--- font/setup/setup-fonts.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-cp $PWD/35-droid.conf /etc/fonts/conf.avail/
-ln -s /etc/fonts/conf.avail/35-droid.conf /etc/fonts/conf.d/
-
-rm /etc/fonts/conf.d/70-yes-bitmaps.conf
-ln -s /etc/fonts/conf.avail/70-no-bitmaps.conf /etc/fonts/conf.d/
