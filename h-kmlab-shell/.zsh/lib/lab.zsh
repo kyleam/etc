2c481c74ce5eb06dacf5d8d7c328d3882acea5c2
Kyle Meyer <kyle@kyleam.com>
2017-07-23 14:48:19 -0400

    Remove stale files

--- h-kmlab-shell/.zsh/lib/lab.zsh
+++
@@ -1,4 +0,0 @@
-PS1="%{$fg[blue]%}[%{$reset_color%}%m: %c%{$fg[blue]%}]%{$reset_color%}%# "
-
-alias onemon="xrandr --output DVI-1 --off"
-alias twomon="xrandr --output DVI-1 --mode 1280x1024 --left-of DVI-0"
